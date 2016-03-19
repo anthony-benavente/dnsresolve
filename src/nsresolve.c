411dcf95f0adc5af3e32148f39ca12943100846a
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-03-19 18:38:09 -0400

    Added code for using the dnsclient

--- src/nsresolve.c
+++ src/nsresolve.c
@@ -3,13 +3,32 @@
 #include <time.h>
 
 #include "dnstypes.h"
+#include "dnsclient.h"
 #include "dnsmsg.h"
 
 int main(void) {
     srand(time(NULL));
-    dnsmsg_t *msg = new_dnsmessage(DNSMSG_OPT_QR | DNSMSG_OPT_RD);
-    dnsmsg_print(msg);
-    dnsmsg_free(msg);
+    
+    uint8_t dns_ip[] = { 8,8,8,8 };
+    dnsmsg_t *query = dnsmsg_create_query("www.google.com", 
+        DNS_TYPE_A, DNS_CLASS_IN);
+    
+    // int i = 1;
+    // int new_ln = 0;
+    // for (; i <= n; i++) {
+    //     printf("%2x ", beginning[i - 1], beginning[i - 1]);
+        
+    //     new_ln = i % 8;
+    //     if (!new_ln) {
+    //         printf("\n");
+    //     }
+    // }
+    // printf("\n");    
+    
+    dnsmsg_t *response = nsclient_resolve(dns_ip, 53, query);
+    
+    dnsmsg_free(query);
+    dnsmsg_free(response);
     
     return 0;
 }
