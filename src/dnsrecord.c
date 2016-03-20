eb11b8884c2dc63c1a6cd633a5fe3a881569b733
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-03-20 00:18:16 -0400

    Fixed dnsrecord still using the params field that was removed

--- src/dnsrecord.c
+++ src/dnsrecord.c
@@ -2,12 +2,16 @@
 #include "wrappers.h"
 
 dnsrecord_t *dnsrecord_new(void) {
-    dnsrecord_t *result = _malloc(sizeof(dnsrecord_t));  
-    result->params = _malloc(sizeof(dnsrecord_t));
+    dnsrecord_t *result = _malloc(sizeof(dnsrecord_t));
+    result->name = NULL;
+    result->type = 0;
+    result->class = 0;
+    result->ttl = 0;
+    result->rdlength = 0;
+    result->rdata = NULL;
     return result;
 }
 
 void dnsrecord_free(dnsrecord_t *record) {
-    free(record->params);
     free(record);
 }
